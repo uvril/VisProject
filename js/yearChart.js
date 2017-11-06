b0437af68c8599fc68fb4b44c651e43f34d823d7
Yanqing Peng <yanqing.utah@gmail.com>
2017-11-05 17:19:09 -0700

    finish yearChart prototype

--- js/yearChart.js
+++ js/yearChart.js
@@ -2,38 +2,67 @@
 class YearChart {
 
     constructor (mapChart) {
-		
-		this.mapChart = mapChart;
 
-var width = 500;
+        this.mapChart = mapChart;
 
-var x = d3.scaleLinear()
-    .domain([1, 100])
-    .range([0, width])
-    .clamp(true);
+        this.svg = d3.select("#yearChart")
+            .append("svg")
+            .attr("height", 100)
+            .attr("width", 1000);
 
-var dispatch = d3.dispatch("sliderChange");
+        let margin = {right:50, left:50};
+        let width = this.svg.attr("width") - margin.left - margin.right;
 
-var slider = d3.select(".slider")
-    .style("width", width + "px");
+        this.x = d3.scaleLinear()
+            .domain([-2001, 2017])
+            .range([0, width])
+            .clamp(true);
 
-var sliderTray = slider.append("div")
-    .attr("class", "slider-tray");
-
-this.sliderHandle = slider.append("div")
-    .attr("class", "slider-handle");
-
-this.sliderHandle.append("div")
-    .attr("class", "slider-handle-icon")
-
-slider.call(d3.drag()
-    .on("end", function() {
-		this.sliderHandle.style("left", d3.event.x+"px");
-		d3.select("#currentYear").text(d3.event.x+1945);
-		this.mapChart.drawMap(d3.event.x+1945);
-    }.bind(this)));
+        this.yearText = this.svg.append("text")
+            .attr("x", 20)
+            .attr("y", 20)
+            .text(this.x.domain()[1]);
 
 
+
+        var slider = this.svg.append("g")
+            .attr("class", "slider")
+            .attr("transform", "translate(" + margin.left + "," + this.svg.attr("height") / 2 + ")");
+
+        slider.append("line")
+            .attr("class", "track")
+            .attr("x1", this.x.range()[0])
+            .attr("x2", this.x.range()[1])
+            .select(function() { return this.parentNode.appendChild(this.cloneNode(true)); })
+            .attr("class", "track-inset")
+            .select(function() { return this.parentNode.appendChild(this.cloneNode(true)); })
+            .attr("class", "track-overlay")
+            .call(d3.drag()
+                .on("start.interrupt", function() { slider.interrupt(); })
+                .on("start drag", function() { this.changeYear(this.x.invert(d3.event.x)); }.bind(this)));
+
+        slider.insert("g", ".track-overlay")
+            .attr("class", "ticks")
+            .attr("transform", "translate(0," + 18 + ")")
+            .selectAll("text")
+            .data(this.x.ticks(30))
+            .enter().append("text")
+            .attr("x", d => this.x(d))
+            .attr("text-anchor", "middle")
+            .text(d => d);
+
+        this.handle = slider.insert("circle", ".track-overlay")
+            .attr("class", "handle")
+            .attr("cx", this.x.range()[1])
+            .attr("r", 9);
+
+    };
+
+    changeYear(h) {
+        this.handle.attr("cx", this.x(h));
+        let year = Math.round(h);
+        this.yearText.text(year);
+        this.mapChart.drawMap(year);
     };
 
 
