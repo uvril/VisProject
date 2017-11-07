ac5c98ee8ef443a391f468e3691324edf9a8d65a
Yanqing Peng <yanqing.utah@gmail.com>
2017-11-06 17:02:24 -0700

    update

--- js/infoPanel.js
+++ js/infoPanel.js
@@ -1,9 +1,25 @@
 class InfoPanel {
-	constructor() {
-	}
+    constructor() {
+    }
 
-	updateInfo(oneCountryInfo, year) {
-		console.log("!!!");
-		console.log(oneCountryInfo);
-	}
+    updateInfo(oneCountryInfo, year) {
+        let endpointUrl = 'https://query.wikidata.org/bigdata/namespace/wdq/sparql',
+            sparqlQuery = "SELECT ?countryLabel ?article WHERE {\n" +
+            "  \n" +
+            "    ?article schema:about wd:Q" + oneCountryInfo.wikidata + " .\n" +
+            "    ?article schema:isPartOf <https://en.wikipedia.org/>.\n" +
+            "\n" +
+            "}",
+            settings = {
+                headers: { Accept: 'application/sparql-results+json' },
+                data: { query: sparqlQuery }
+            };
+
+        $.ajax( endpointUrl, settings ).then( function ( data ) {
+            $( 'body' ).append( ( $('<pre>').text( JSON.stringify( data) ) ) );
+            console.log( data.results.bindings[0].article.value );
+        } );
+
+
+    }
 }
