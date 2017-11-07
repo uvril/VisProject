6254f338296db4f07fdb38f655a6d49c31feb5f8
uvril <yuweiwang.uvril@gmail.com>
2017-11-06 21:38:43 -0700

    update infoPanel

--- js/infoPanel.js
+++ js/infoPanel.js
@@ -1,5 +1,6 @@
 class InfoPanel {
     constructor() {
+    	this.tmp = 0;
     }
 
     updateInfo(oneCountryInfo, year) {
@@ -16,10 +17,13 @@ class InfoPanel {
             };
 
         $.ajax( endpointUrl, settings ).then( function ( data ) {
-            $( 'body' ).append( ( $('<pre>').text( JSON.stringify( data) ) ) );
-            console.log( data.results.bindings[0].article.value );
-        } );
+            //$( 'body' ).append( ( $('<pre>').text( JSON.stringify( data) ) ) );
+            //this.tmp = data.results.bindings[0].article.value;
+            document.getElementById("wikipage").setAttribute("src", data.results.bindings[0].article.value+"?printable=yes");
 
+        }.bind(this));
 
+        //console.log(this.tmp);
+        //document.getElementById("wikipage").innerHTML = "<iframe height=\"1000\" width=\"1000\" src="+this.tmp+"?printable=yes></iframe>";
     }
 }
