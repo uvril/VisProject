7b0b3c964e6eabdf30deaf881489010bd6c478ab
uvril <yuweiwang.uvril@gmail.com>
2017-11-07 20:49:52 -0700

    add humanindex

--- js/infoPanel.js
+++ js/infoPanel.js
@@ -4,6 +4,8 @@ class InfoPanel {
     }
 
     updateInfo(oneCountryInfo, year) {
+        document.getElementById("country").innerHTML = oneCountryInfo.NAME;
+
         console.log(oneCountryInfo.wikidata);
         let endpointUrl = 'https://query.wikidata.org/bigdata/namespace/wdq/sparql',
             sparqlQuery = "SELECT ?countryLabel ?article WHERE {\n" +
@@ -20,13 +22,13 @@ class InfoPanel {
             document.getElementById("wikipage").setAttribute("src", data.results.bindings[0].article.value+"?printable=yes");
         });
 
-        let labelPQuery = function (wikidata, domain) {
+        let labelQuery = function (wikidata, domain) {
             let sparqlQuery = "SELECT ?name WHERE {\n" +
             "    wd:Q" + wikidata + " wdt:P" + domain +" ?link.\n" +
             "    ?link rdfs:label ?name.\n" +
             "    filter (lang(?name) = \"en\")\n" +
             "}";
-            settings = {
+            let settings = {
                 headers: { Accept: 'application/sparql-results+json' },
                 data: { query: sparqlQuery }
             };
@@ -34,12 +36,12 @@ class InfoPanel {
         }
 
         let domain = 36;
-        $.ajax( endpointUrl, labelPQuery(oneCountryInfo.wikidata, domain.toString()) ).then( function ( data ) {
+        $.ajax( endpointUrl, labelQuery(oneCountryInfo.wikidata, domain.toString()) ).then( function ( data ) {
             document.getElementById("capital").innerHTML = data.results.bindings[0].name.value;
         });
 
         domain = 30;
-        $.ajax( endpointUrl, labelPQuery(oneCountryInfo.wikidata, domain.toString()) ).then( function ( data ) {
+        $.ajax( endpointUrl, labelQuery(oneCountryInfo.wikidata, domain.toString()) ).then( function ( data ) {
             d3.select("#continent")
                 .html("");
             d3.select("#continent")
@@ -50,24 +52,35 @@ class InfoPanel {
                 .text(d=>d.name.value);
         });
 
-        domain = 6;
-        $.ajax( endpointUrl, labelPQuery(oneCountryInfo.wikidata, domain.toString()) ).then( function ( data ) {
-            document.getElementById("head").innerHTML = data.results.bindings[0].name.value;
+        domain = 35;
+        $.ajax( endpointUrl, labelQuery(oneCountryInfo.wikidata, domain.toString()) ).then( function ( data ) {
+            document.getElementById("headState").innerHTML = data.results.bindings[0].name.value;
         });
 
-        sparqlQuery = "SELECT ?population ?year WHERE {\n" +
-        "    wd:Q" + oneCountryInfo.wikidata + " p:P1082 ?poplink.\n" +
-        "    ?poplink ps:P1082 ?population.\n" +
-        "    ?poplink pq:P585 ?date.\n" +
-        "    bind (year(?date) as ?year).\n" +
-        "}\n" +
-        "order by ?year\n",
-        settings = {
-            headers: { Accept: 'application/sparql-results+json' },
-            data: { query: sparqlQuery }
-        };
-        console.log(sparqlQuery);
-        $.ajax( endpointUrl, settings ).then( function ( data ) {
+        domain = 1081;
+        $.ajax( endpointUrl, labelQuery(oneCountryInfo.wikidata, domain.toString()) ).then( function ( data ) {
+            console.log(data);
+            document.getElementById("humanIndex").innerHTML = data.results.bindings[0].name.value;
+        });
+
+        let statsQuery = function(wikidata, domain) {
+            let sparqlQuery = "SELECT ?stats ?year WHERE {\n" +
+            "    wd:Q" + wikidata + " p:P" + domain + " ?poplink.\n" +
+            "    ?poplink ps:P" + domain +" ?stats.\n" +
+            "    ?poplink pq:P585 ?date.\n" +
+            "    bind (year(?date) as ?year).\n" +
+            "}\n" +
+            "order by ?year\n";
+            let settings = {
+                headers: { Accept: 'application/sparql-results+json' },
+                data: { query: sparqlQuery }
+            };
+            return settings;
+        }
+
+        domain = 1082;
+        $.ajax( endpointUrl, statsQuery(oneCountryInfo.wikidata, domain.toString()) ).then( function ( data ) {
+            console.log(data);
             d3.select("#population")
                 .html("");
             d3.select("#population")
@@ -75,7 +88,19 @@ class InfoPanel {
                 .selectAll("li")
                 .data(data.results.bindings)
                 .enter().append("li")
-                .text(d=>d.year.value+"  "+d.population.value);
+                .text(d=>d.year.value+"  "+d.stats.value);
+        });
+
+        domain = 1081;
+        $.ajax( endpointUrl, statsQuery(oneCountryInfo.wikidata, domain.toString()) ).then( function ( data ) {
+            d3.select("#humanIndex")
+                .html("");
+            d3.select("#humanIndex")
+                .append("ul")
+                .selectAll("li")
+                .data(data.results.bindings)
+                .enter().append("li")
+                .text(d=>d.year.value+"  "+d.stats.value);
         });
     }
 }
