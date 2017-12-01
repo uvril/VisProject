58726764d6c3a4f97807cf50bd72af6ec980e970
uvril <yuweiwang.uvril@gmail.com>
2017-11-30 23:36:55 -0700

    finish static rankview

--- js/rankView.js
+++ js/rankView.js
@@ -12,7 +12,7 @@ class RankView {
 		let wd = "148";
 		let data = window.dataset[this.category]["2014"];
 		let metaInfo = [];
-		let barWidth = 15, barHeight = 200;
+		let barWidth = 8, barHeight = 120;
 		
 		for (let i in data) {
 			metaInfo.push({
@@ -21,11 +21,12 @@ class RankView {
 			})
 		}
 		metaInfo.sort(function(a, b) { return d3.descending(a.stats, b.stats); });
-		let wdRank = -1;
-		metaInfo.forEach(function(d, i){
-			if (d.wd === wd) wdRank = i+1;
-			d.rank = i+1;
-		});
+		metaInfo = metaInfo.splice(0, 41);
+		let thisRank = -1;
+		for (let i in metaInfo) {
+			if (metaInfo[i].wd === wd) thisRank = +i+1;
+			metaInfo[i].rank = +i+1;
+		}
 		let maxData = d3.max(metaInfo, d=>d.stats);
 		console.log(metaInfo);
 		let groups = this.rankView.selectAll("g")
@@ -40,16 +41,38 @@ class RankView {
 				.style("fill", "AliceBlue");
 
 		let areaGenerator = d3.area()
-								.x((d)=>d.stats/maxData*barHeight)
-								.y1((d, i)=>i*barWidth);
+								.x0(0)
+								.x1(function(d){
+									console.log(d.stats/maxData*barHeight);
+									return d.stats/maxData*barHeight;
+								})
+								.y(function(d, i){
+									console.log(i*barWidth, i);
+									return i*barWidth;
+								});
 
 		groups.append("path")
 				.style("fill", "CornflowerBlue")
 				.attr("d", areaGenerator)
 				.on("mousemove", function(){
-					console.log(d3.event, d3.event.mouseY/barWidth, metaInfo[Math.floor(d3.event.mouseY/barWidth)]);
+					console.log(d3,event, metaInfo[Math.floor(d3.event.layerY/barWidth)]);
 				});
 
+		groups.append("circle")
+				.attr("cx", metaInfo[thisRank-1].stats/maxData*barHeight)
+				.attr("cy", (thisRank-1)*barWidth)
+				.attr("r", 5)
+				.style("fill", "yellow")
+				.style("fill-opacity", 0.5)
+				.style("stroke", "black");
+
+		groups.append("line")
+				.attr("x1", 0)
+				.attr("x2", metaInfo[thisRank-1].stats/maxData*barHeight-5)
+				.attr("y1", (thisRank-1)*barWidth)
+				.attr("y2", (thisRank-1)*barWidth)
+				.style("stroke", "black")
+				.style("stroke-width", "2px")
 
 	}
 }
