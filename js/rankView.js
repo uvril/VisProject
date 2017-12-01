dee592d7c833ac85b3ca915cca6118bf22813f68
Yuwei Wang <yuweiwang.uvril@gmail.com>
2017-12-01 16:06:12 -0700

    rankView completed

--- js/rankView.js
+++ js/rankView.js
@@ -1,7 +1,7 @@
 class RankView {
 	constructor() {
 		this.svgWidth = 800;
-		this.svgHeight = 400;
+		this.svgHeight = 500;
 		this.rankView = d3.select("#rankView")
 							.attr("width", this.svgWidth)
 							.attr("height", this.svgHeight);
@@ -16,7 +16,7 @@ class RankView {
 		console.log(wd, countryName)
 		console.log(year);
 		let metaInfo = [];
-		let barWidth = 8, barHeight = 120, chartPadding = 100, selectedNum = 41;
+		let barWidth = 8, barHeight = 120, chartPadding = 100, selectedNum = 51, textPos = 15;
 		this.category.forEach(function(category){
 			let data = window.dataset[category][year];
 			let metaG = []
@@ -29,10 +29,11 @@ class RankView {
 			}
 			metaInfo.push(metaG);
 		})
-		for (let i = 0; i < metaInfo.length-1; ++i) {
-			metaInfo[i].sort(function(a, b) { return d3.descending(a.stats, b.stats); });
+		for (let i = 0; i < metaInfo.length; ++i) {
+			metaInfo[i].sort(function(a, b) { return d3.descending(+a.stats, +b.stats); });
 			metaInfo[i] = metaInfo[i].splice(0, selectedNum);
 		};
+		console.log(metaInfo);
 		let thisRank = [];
 		for (let i in metaInfo) {
 			let flag = false;
@@ -56,55 +57,57 @@ class RankView {
 									.data(metaInfo);
 
 		groups = groups.enter().append("g").merge(groups);
-		groups.attr("transform", (d,i)=>"translate("+i*(barHeight+chartPadding)+",10)");
+		groups.attr("transform", (d,i)=>"translate("+i*(barHeight+chartPadding)+","+ (textPos*2) +")");
 		groups.html("");
 
-		/*let tipShow = function(event) {
+		let tipShow = function(mouseevent, category) {
             this.rankTip.transition()
                 .duration(200)
                 .style("opacity", .9);
-            let curRank = Math.floor(event.layerY/barWidth), start = curRank-3, end = curRank+3;
+            let curRank = Math.floor(mouseevent[1]/barWidth), start = curRank-3, end = curRank+3;
             if (start < 0) {
             	start = 0;
             	end = 6;
             }
-            if (end > metaInfo.length-2){
-            	end = metaInfo.length-2;
-            	start = metaInfo.length-2-6;
+            //console.log(category);
+            if (end > metaInfo[category].length-2){
+            	end = metaInfo[category].length-2;
+            	start = metaInfo[category].length-2-6;
             }
             let html = "";
             for (let i = start; i <= end; ++i) {
             	let star = "";
-            	if (i == thisRank-1) 
+            	if (i == thisRank[category]-1) 
             		star = "❤";
             	if (i == curRank) 
-            		html += "<font size=\"4\"><b>#" + (i+1).toString() + " " + wdMap[metaInfo[i].wd] + " (" + d3.format(".2s")(metaInfo[i].stats) + ") " + star + "<br><\/b>" + "<\/font>";
+            		html += "<font size=\"4\"><b>#" + (i+1).toString() + " " + wdMap[metaInfo[category][i].wd] + " (" + d3.format(".2s")(metaInfo[category][i].stats) + ") " + star + "<br><\/b>" + "<\/font>";
             	else if (i == curRank-1 || i == curRank+1) 
-            		html += "<font size=\"3\">#" + (i+1).toString() + " (" + wdMap[metaInfo[i].wd] + " " + d3.format(".2s")(metaInfo[i].stats) + ") " + star + "<br><\/font>";
-            	else html += "<font size=\"2\">#" + (i+1).toString() + " (" + wdMap[metaInfo[i].wd] + " " + d3.format(".2s")(metaInfo[i].stats) + ") " + star + "<br><\/font>";
+            		html += "<font size=\"3\">#" + (i+1).toString() + " " + wdMap[metaInfo[category][i].wd] + " (" + d3.format(".2s")(metaInfo[category][i].stats) + ") " + star + "<br><\/font>";
+            	else html += "<font size=\"2\">#" + (i+1).toString() + " " + wdMap[metaInfo[category][i].wd] + " (" + d3.format(".2s")(metaInfo[category][i].stats) + ") " + star + "<br><\/font>";
             }
             this.rankTip.html(html)
                 .style("left", (event.pageX) + "px")
-                .style("top", (event.pageY-28) + "px");
+                .style("top", (event.pageY-60) + "px");
 		}.bind(this);
 
 		let tipUnshow = function() {
             this.rankTip.transition()
                 .duration(500)
                 .style("opacity", 0);
-		}.bind(this);*/
+		}.bind(this);
 
 		groups.append("rect")
+				.attr("id", "testrectrect")
 				.attr("x", 0)
 				.attr("y", 0)
 				.attr("height", (d,i)=>barWidth*(selectedNum-1))
 				.attr("width", (d,i)=>maxData[i]!=-1?barHeight:0)
 				.style("fill", "AliceBlue")
-				.on("mousemove", function(){
-					//tipShow(d3.event);
+				.on("mousemove", function(d, i){
+					tipShow(d3.mouse(this), i);
 				})
 				.on("mouseout", function(){
-					//tipUnshow();
+					tipUnshow();
 				});
 
 		let areaGenerator = d3.area(maxData)
@@ -123,11 +126,11 @@ class RankView {
 		groups.append("path")
 				.style("fill", "CornflowerBlue")
 				.attr("d", areaGenerator)
-				.on("mousemove", function(){
-					//tipShow(d3.event);
+				.on("mousemove", function(d, i){
+					tipShow(d3.mouse(this), i);
 				})
 				.on("mouseout", function(){
-					//tipUnshow();
+					tipUnshow();
 				});
 
 		groups.append("circle")
@@ -137,25 +140,36 @@ class RankView {
 				.style("fill", "yellow")
 				.style("fill-opacity", 0.5)
 				.style("stroke", "black")
-				.on("mousemove", function(){
-					//tipShow(d3.event);
-				})
-				.on("mouseout", function(){
-					//tipUnshow();
-				});
-
-		/*groups.append("line")
-				.attr("x1", 0)
-				.attr("x2", metaInfo[thisRank-1].stats/maxData*barHeight-5)
-				.attr("y1", (thisRank-1)*barWidth)
-				.attr("y2", (thisRank-1)*barWidth)
-				.style("stroke", "black")
-				.style("stroke-width", "2px")
-				.on("mousemove", function(){
-					tipShow(d3.event);
+				.on("mousemove", function(d, i){
+					tipShow(d3.mouse(this), i);
 				})
 				.on("mouseout", function(){
 					tipUnshow();
-				});*/
+				});
+
+		groups.append("line")
+				.attr("x1", 0)
+				.attr("x2", (d, i)=>thisRank[i] !=-1? metaInfo[i][thisRank[i]-1].stats/maxData[i]*barHeight-5:0)
+				.attr("y1", (d, i)=>thisRank[i] !=-1? (thisRank[i]-1)*barWidth:0)
+				.attr("y2", (d, i)=>thisRank[i] !=-1? (thisRank[i]-1)*barWidth:0)
+				.style("stroke", "black")
+				.style("stroke-width", "2px")
+				.on("mousemove", function(d, i){
+					tipShow(d3.mouse(this), i);
+				})
+				.on("mouseout", function(){
+					tipUnshow();
+				});
+
+		let textG = this.rankView.selectAll("text")
+					.data(this.category);
+		textG = textG.enter().append("text").merge(textG);
+		textG.attr("x", (d,i)=>maxData[i]!=-1?i*(barHeight+chartPadding)+barHeight/2:0)
+			.attr("y", textPos)
+			.text(function(d, i){
+				if (maxData[i]!=-1) return this.category[i];
+				return "";
+			}.bind(this))
+			.style("text-anchor", "middle")
 	}
 }
