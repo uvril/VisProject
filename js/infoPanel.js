f100ffff8571c2201401b1f50106f77634012240
uvril <yuweiwang.uvril@gmail.com>
2017-12-01 21:24:31 -0700

    add religions donut

--- js/infoPanel.js
+++ js/infoPanel.js
@@ -3,6 +3,9 @@ class InfoPanel {
         this.contentObj = d3.select("#details");
         this.infoTable = this.contentObj.select("#basicInfoTable");
         this.aggPanel = aggPanel;
+        this.donut = d3.select("#donut")
+                        .attr("height", 200)
+                        .attr("width", 200);
     }
 
     updateInfo(oneCountryInfo, year) {
@@ -17,31 +20,60 @@ class InfoPanel {
             this.contentObj.select("#countryInfo").style("visibility", "visible");
         }
         d3.json("data/stat/" + wd + ".json", function(err, data) { 
-         
-            this.infoTable.select("#table-capital").html(data.capital[0]);
-            this.infoTable.select("#table-continent").html(data.continent.join(', '));
-            this.infoTable.select("#table-hos").html(data.headState[0]);
-            this.infoTable.select("#table-hog").html(data.headGov[0]);
-            if (data.pop.length > 0) {
-                let latestPopYear = d3.max(data.pop, d => +d.year);
-                let latestPop = data.pop.filter(d => +d.year === latestPopYear)[0]
-                this.infoTable.select("#table-population").html(latestPop.stats+" ("+latestPop.year+")");
-            }
-            else {
-                this.infoTable.select("#table-population").html("");
-            }
-            if (data.hdi.length > 0) {
-                let latestHDIYear = d3.max(data.hdi, d => +d.year);
-                let latestHDI = data.hdi.filter(d => +d.year === latestHDIYear)[0]
-                this.infoTable.select("#table-hdi").html(latestHDI.stats+" ("+latestHDI.year+")");
-            }
-            else {
-                this.infoTable.select("#table-hdi").html("");
-            }
-            d3.select("#add-button")
-                .on("click", function(){
-                    this.aggPanel.insertCountry(oneCountryInfo.NAME, wd);
-                }.bind(this));
+            d3.json("data/religion.json", function(err, rdata){
+                this.infoTable.select("#table-capital").html(data.capital[0]);
+                this.infoTable.select("#table-continent").html(data.continent.join(', '));
+                this.infoTable.select("#table-hos").html(data.headState[0]);
+                this.infoTable.select("#table-hog").html(data.headGov[0]);
+                if (data.pop.length > 0) {
+                    let latestPopYear = d3.max(data.pop, d => +d.year);
+                    let latestPop = data.pop.filter(d => +d.year === latestPopYear)[0]
+                    this.infoTable.select("#table-population").html(latestPop.stats+" ("+latestPop.year+")");
+                }
+                else {
+                    this.infoTable.select("#table-population").html("");
+                }
+                if (data.hdi.length > 0) {
+                    let latestHDIYear = d3.max(data.hdi, d => +d.year);
+                    let latestHDI = data.hdi.filter(d => +d.year === latestHDIYear)[0]
+                    this.infoTable.select("#table-hdi").html(latestHDI.stats+" ("+latestHDI.year+")");
+                }
+                else {
+                    this.infoTable.select("#table-hdi").html("");
+                }
+
+                console.log(rdata);
+                let donutData = rdata[wd], seletedNum = 5;
+
+                let pie = d3.pie()
+                            .value(d=>d.tot)
+                let color = ["#1f77b4","#aec7e8","#ff7f0e","#ffbb78","#2ca02c","#98df8a","#d62728","#ff9896","#9467bd","#c5b0d5","#8c564b","#c49c94","#e377c2","#f7b6d2","#7f7f7f","#c7c7c7","#bcbd22","#dbdb8d","#17becf"];
+                this.donut.html("");
+                let group = this.donut.append("g")
+                            .attr("transform", "translate(100, 100)")
+                            .data([donutData]);
+                let paths = group.selectAll("path")
+                            .data(function(d){
+                                return pie(d);
+                            });
+                            
+                let arc = d3.arc()
+                            .innerRadius(100)
+                            .outerRadius(80);
+                paths = paths.enter().append("path").merge(paths)
+                            .filter(d=>d.data.tot != 0)
+                            .attr("id", d=>d.data.religion)
+                            .style("fill", (d, i)=>color[i]);
+                paths.attr("d", arc);
+                console.log("!!!");
+
+
+
+                d3.select("#add-button")
+                    .on("click", function(){
+                        this.aggPanel.insertCountry(oneCountryInfo.NAME, wd);
+                    }.bind(this));
+            }.bind(this))
         }.bind(this));
 
     }
