4e4c8f2129487639d834def0bf7e3543362fe868
Yanqing Peng <yanqing.utah@gmail.com>
2017-12-02 19:01:51 -0700

    update

--- data_collection/event.py
+++ data_collection/event.py
@@ -2,50 +2,51 @@ import requests
 import re
 from html.parser import HTMLParser
 import json
+import csv
 parser = HTMLParser()
 
 def query(country, wd, data):
-	
-	pn = 1
-	cur_pn = 1
-	while cur_pn <= pn:
-		html = requests.get('https://www.onthisday.com/countries/%s?p=%d' % (country, cur_pn))
-		cur_pn += 1
-	
-		for line in html.text.split('\n'):
-			if line == """<header class="section__heading"><h2>Famous Birthdays</h2></header>""":
-				break
-			findre = re.findall(r'(\d\d\d\d-\d\d-\d\d)', line)
-			if len(findre) > 0:
-				str = line[:]
-				str = re.sub(r'<script>[^<]+</script>', "", str)
-				str = re.sub('<[^<]+>', "", str)
-				str = parser.unescape(str)
-				
-				if len(re.findall(r' BC ', str)) == 0:
-					yy = int(str[:4])
-					if yy>1940:
-						ee = str[11:]
-						eed = {"wd" : wd, "event" : ee}
-						if not yy in data:
-							data[yy] = [eed]
-						else:
-							data[yy].append(eed)
-			s = re.findall("""<a href="/countries/%s\?p=[\d]+""" % country, line)
-			if len(s) > 0:
-				t_pn = int(re.findall("[\d]+", s[0])[0])
-				if t_pn > pn:
-					pn = t_pn
-	
+    
+    pn = 1
+    cur_pn = 1
+    while cur_pn <= pn:
+        html = requests.get('https://www.onthisday.com/countries/%s?p=%d' % (country, cur_pn))
+        cur_pn += 1
+    
+        for line in html.text.split('\n'):
+            if line == """<header class="section__heading"><h2>Famous Birthdays</h2></header>""":
+                break
+            findre = re.findall(r'(\d\d\d\d-\d\d-\d\d)', line)
+            if len(findre) > 0:
+                str = line[:]
+                str = re.sub(r'<script>[^<]+</script>', "", str)
+                str = re.sub('<[^<]+>', "", str)
+                str = parser.unescape(str)
+                
+                if len(re.findall(r' BC ', str)) == 0:
+                    yy = int(str[:4])
+                    if yy>1940:
+                        ee = str[11:]
+                        eed = {"wd" : wd, "event" : ee, "time" : str[:11]}
+                        if not yy in data:
+                            data[yy] = [eed]
+                        else:
+                            data[yy].append(eed)
+            s = re.findall("""<a href="/countries/%s\?p=[\d]+""" % country, line)
+            if len(s) > 0:
+                t_pn = int(re.findall("[\d]+", s[0])[0])
+                if t_pn > pn:
+                    pn = t_pn
+    
+data={}
 
-cl = open("cl.txt", "r")
-data = {}
-for cll in cl:
-	acl = cll.strip().split()
-	cn = acl[0]
-	wd = int(acl[1])
-	query(cn, wd, data)
+with open('wd.csv', 'r') as f:
+    reader = csv.reader(f)
+    for acl in reader:
+        cn = acl[0]
+        wd = int(acl[2])
+        query(cn, wd, data)
 
-f = open("events.txt", "w", encoding="utf-8")
+f = open("events.json", "w", encoding="utf-8")
 json.dump(data, f)
 f.close()
